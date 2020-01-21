bcd222b0546e08aa4e22fe54acb46f096c441ab6
Jesús Martínez Torrecilla <jesusmartorrecilla@gmail.com>
2020-01-21 01:37:20 +0100

    created DB

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const http = require('http').Server(express);
 const Socketeio = require('socket.io')(http);
+const mongodb = require('mongodb');
 
 
+const config = require('./db');
+const client = mongodb.MongoClient;
 // Constants
 const PORT = 8000;
 const HOST = 'localhost';
 
+
+//SOCKET IO
 Socketeio.on("connection", socket => {
-  socket.emit("holo", 123)
+  socket.emit("holo", 123);
+
+  socket.on('test', function(msg){
+    console.log('message: ' + msg);
+  });
 });
+
+//MONGO DB
+
+client.connect(config.DB, function(err, db) {
+  if(err) {
+      console.log('database is not connected')
+  }
+  else {
+      console.log('connected!!')
+  }
+});
+
 
 
 http.listen(PORT, () => {
---
+++ db.js
@@ -0,0 +1,5 @@
+// db.js
+
+module.exports = {
+    DB: 'mongodb://db:27017/newdb'
+}
